583abf7a67d0164a2dd40b5082e3cdb5a9dd9f1a
Gaurav Kumar <97530398+Gaurav01Kumar@users.noreply.github.com>
2022-11-27 14:15:38 +0530

    my portfolio

---
+++ hello.py
@@ -0,0 +1,55 @@
+import pyautogui 
+import time
+time.sleep(4)
+count=0
+while count <=1000:
+    pyautogui.typewrite("I Love you" + str(count))
+    pyautogui.press("enter")
+    count=count+1I Love you43
+    I Love you44
+    I Love you45
+    I Love you46
+    I Love you47
+    I Love you48
+    I Love you49
+    I Love you50
+    I Love you51
+    I Love you52
+    I Love you53
+    I Love you54
+    I Love you55
+    I Love you56
+    I Love you57
+    I Love you58
+    I Love you59
+    I Love you60
+    I Love you61
+    I Love you62
+    I Love you63
+    I Love you64
+    I Love you94
+    I Love you95 you97
+    I Love you98
+    I Love you99I Love you130
+    I Love you131
+    I Love you132
+    I Love you133
+    
+    
+I Love you146
+I Love you147
+I Love you148
+I Love you149
+I Love you150
+    
+I Love you190
+I Love you191
+I Love you192
+I Love you193
+I Love you194
+I Love you195
+    I Love you100
+    pip install pyautogui
+    I Love you129
+    I Love you96
+    I Love
