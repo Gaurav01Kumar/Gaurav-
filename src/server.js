739059292f6c9a5a46b8e00e31ef0f3624ae44c1
Gaurav Kumar <97530398+Gaurav01Kumar@users.noreply.github.com>
2023-07-11 08:20:11 +0530

    a some major change

--- src/server.js
+++ src/server.js
@@ -2,18 +2,39 @@ const express=require("express");
 const app=express();
 const bodyParser=require("body-parser")
 const port = process.env.PORT || 3000;
-
+//const nodemailer=require("nodemailer")
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"));
 app.set("view engine","ejs")
 // get route for home page 
+
+// const transporter = nodemailer.createTransport({
+//   host: 'gmail',
+//   port: 465,
+  
+//   auth: {
+//       user: 'gk1427473@gmail.com',
+//       pass: 'Gaurav@9128Kumar'
+//   }
+// });
+
+// const mail=transporter.sendMail({
+//   from:'"gaurav"<sharon30@ethereal.email>',
+//   to:'gaurav1raj@gmail.com',
+//   subject:"Hello world",
+//   text:"node  " 
+// })
+// if(mail){
+//   console.log("successfully save",mail)
+// }
+
 app.get("/",(req,res)=>{
     res.render("index")
 })
 
 // get route for contact page
-app.get("/Contact",(req,res)=>{
+app.get("/contact",(req,res)=>{
   res.render("Contact")
 })
   
